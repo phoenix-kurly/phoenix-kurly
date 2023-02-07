94aeb7c7c715b148193b5c4319938295a3d5108b
yurimpark <yuriming112@gmail.com>
2023-02-07 16:29:50 +0900

    [추가] navbar is-active 클래스 탈부착

--- productDetail.js
+++ productDetail.js
@@ -1,5 +1,17 @@
+// 찜 클릭시 하트 변경
 let toggle = false;
 function toggleLike() {
   document.getElementById('like').src = toggle ? './client/assets/icons/icon/redHeart.svg' : './client/assets/icons/icon/Heart.svg';
   toggle = !toggle;
 }
+
+// navbar 선택 is-active 클래스 탈부착
+const items = document.querySelectorAll('.list');
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    items.forEach(e => {
+      e.classList.remove('is-active');
+    });
+    item.classList.add('is-active');
+  });
+});
