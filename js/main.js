f5057ed7c0145c7eabb45c2950314135a1d76d1a
baeseongjae <3574214@naver.com>
2023-02-07 18:38:09 +0900

    [추가] 장바구니 모달 창 일부 기능 구현 (창 열기/닫기, 상품 개수 증감)

--- js/main.js
+++ js/main.js
@@ -1,46 +1,5 @@
+import "./swiper.js";
+import "./modal.js";
 
-const swiper = new Swiper('.swiper-container',{
-  autoplay:true,
-  loop:true,
-  speed: 1000,
-  parallax:true,
-  pagination:{
-    el: '.pagination',
-    clickable: true, 
-    type: 'fraction'
-  },
-  // 동적로딩
-  lazy:{
-    loadPrevNext: true //이전, 다음 이미지 미리 로딩
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  
-});
+// import { addClass, getNode, removeClass } from '../client/lib';
 
-new Swiper('.today-product-swiper-container',{
-  autoplay: false,
-  loop: true,
-  lazy:{
-    loadPrevNext:true
-  },
-  navigation: {
-    nextEl: '.today-product-next',
-    prevEl: '.today-product-prev'
-  }
-});
-
-
-new Swiper('.product-swiper-container',{
-  autoplay: false,
-  loop: true,
-  lazy:{
-    loadPrevNext:true
-  },
-  navigation: {
-    nextEl: '.product-button-next',
-    prevEl: '.product-button-prev'
-  }
-});
