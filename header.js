2718d6450a1ce0fcd0487ddd0bc078727ed79524
hungerbk <wjantlr@gmail.com>
2023-02-07 22:08:31 +0900

    [수정] 메인 브랜치 병합 충돌 수정

---
+++ header.js
@@ -0,0 +1,30 @@
+const category = document.getElementById("category");
+const list = document.getElementById("list");
+const dcAds = document.getElementById("dcAds");
+const closeBtn = document.getElementById("closeBtn");
+// const colorSwitch = document.querySelector(".page-switch");
+
+function openListHandler(){
+  list.classList.remove('hidden');
+}
+
+function closeListHandler(){
+  list.classList.add('hidden');
+}
+
+function closeBannerHandler(){
+  dcAds.classList.add('hidden');
+}
+
+// function colorChangeHandler(event){
+//   let pageSwitch = event.target;
+//   pageSwitch.classList.add("primary-color");
+// }
+
+category.addEventListener('mouseenter', openListHandler);
+list.addEventListener('mouseleave', closeListHandler);
+
+closeBtn.addEventListener('click', closeBannerHandler);
+
+// colorSwitch.addEventListener('click', colorChangeHandler)
+
