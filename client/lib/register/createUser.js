8ba7cd7ab063f1fd22294effebbff5cda51773ac
hungerbk <wjantlr@gmail.com>
2023-02-08 23:58:48 +0900

    [수정] 회원가입 기능 구현 - email, pw 유효성, 회원가입 완료 후 페이지 이동

--- client/lib/register/createUser.js
+++ client/lib/register/createUser.js
@@ -1,5 +1,8 @@
 import {getNode} from '../dom/getNode.js';
 import {user} from '../utils/user.js';
+import {checkEmail} from './checkEmail.js';
+import {makeUniqueId} from './makeRandom.js';
+import {matchPw, checkLength} from './validPw.js';
 
 const register = getNode('#register-form');
 
@@ -8,10 +11,20 @@ export async function createUser() {
   try {
     let formField = new FormData(form);
     let formData = Object.fromEntries(formField.entries());
-    console.log(formData);
-    let response = await user.post('http://localhost:3000/users', formData);
-    console.log(response);
+    if (checkEmail() === false) {
+      alert('이메일을 확인해주세요.');
+    } else if (matchPw() === false || checkLength() === false) {
+      alert('비밀번호를 확인해주세요.');
+    } else {
+      let response = await user.post('http://localhost:3000/users', {formData, uniqueId: makeUniqueId()});
+      if (response) {
+        alert('회원가입이 완료되었습니다!');
+        location.href = '../../../index/index.html';
+      }
+    }
   } catch (error) {
     console.log(error);
+    alert('회원가입에 실패했습니다.');
+    location.reload(true);
   }
 }
